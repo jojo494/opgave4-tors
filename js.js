f1fd36124b10bf3d782abb616064a762ea195382
jojo494 <jowana_sh_@hotmail.com>
2024-11-27 14:35:24 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+// function Trafficlights(){
+//     const red = document.getElementById('red');
+//     const yellow =document.getElementById('yellow');
+//     const green = document.getElementById('green');
+// }
+
+//     function Red(){
+//         red.style.backgroundColor='red';
+//         yellow.style.backgroundColor='yellow';
+//         green.style.backgroundColor='green';
+//         setTimeout(Yellow,2000);
+    
+//     }
+
+//     function Yellow(){
+//         red.style.backgroundColor='red';
+//         yellow.style.backgroundColor='yellow';
+//         green.style.backgroundColor='green';
+//         setTimeout(Green,2000);
+//     }
+
+//     function Green(){
+//         red.style.backgroundColor='red';
+//         yellow.style.backgroundColor='yellow';
+//         green.style.backgroundColor='green';
+//         setTimeout(Red,2000); 
+//     }
+//     Red();
+
+//     Trafficlights();
+
+
+function brightRed(){
+    document.getElementById("red").style.color = 'red';
+}
